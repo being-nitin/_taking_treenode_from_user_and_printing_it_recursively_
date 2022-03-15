faf199c8d54aad257672b873348bd29e49845212
Nitin <nsinghboss02@gmail.com>
2022-03-15 22:08:29 +0530

    Initial commit

---
+++ src/com/Recursion/Treeuse.java
@@ -0,0 +1,35 @@
+package com.Recursion;
+import java.util.ArrayList;
+
+import java.util.Scanner;
+public class Treeuse {
+
+    public static TreeNode<Integer> takeInput(){
+        int n;
+        // n will the the value of corresponding root node of every Treenode;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        TreeNode<Integer> root = new TreeNode<>(n);
+
+        // now the number of child,every node will have:
+
+        int childCount;
+        System.out.println();
+        childCount = sc.nextInt();
+        for(int i=0;i<childCount;i++){
+            TreeNode<Integer> child = takeInput();
+            root.children.add(child);
+        }
+        return root;
+    }
+    public static void print(TreeNode<Integer> root){
+        String s = root.data + ":";
+        for(int i=0;i<root.children.size();i++){
+            s = s+ root.children.get(i).data+",";
+        }
+    }
+    public static void main(String args[]){
+        TreeNode<Integer> root = takeInput();
+        print(root);
+    }
+}
